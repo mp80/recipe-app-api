80d39e2011db0b20b80d95d6557ed76b00267a43
Mike <mpatrick80@gmail.com>
2020-09-05 17:12:29 +0200

    Updated tests for user upadte

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -11,7 +11,6 @@ ME_URL = reverse('user:me')
 
 
 def create_user(**params):
-    """Helper function to create new user"""
     return get_user_model().objects.create_user(**params)
 
 
@@ -24,31 +23,32 @@ class PublicUserApiTests(TestCase):
     def test_create_valid_user_success(self):
         """Test creating user with valid payload is successful"""
         payload = {
-            'email': 'test@londonappdev.com',
-            'password': 'testpass',
-            'name': 'Test name'
+            'email': 'vasas@cisco.com',
+            'password': 'password123',
+            'name': 'Vasanth',
         }
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         user = get_user_model().objects.get(**res.data)
-        self.assertFalse(user.check_password(payload['password']))
+        self.assertTrue(user.check_password(payload['password']))
         self.assertNotIn('password', res.data)
 
     def test_user_exists(self):
-        """Test creating a user that already exists fails"""
-        payload = {'email': 'test@londonappdev.com', 'password': 'testpass', 'name': 'test'}
+        """test creating a user that already exists"""
+        payload = {'email': 'vasas@cisco.com', 'password': 'testpass', 'name': 'Vasanth'}
         create_user(**payload)
+
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_password_too_short(self):
         """Test that password must be more than 5 characters"""
-        payload = {'email': 'test@londonappdev.com', 'password': 'pw', 'name': 'Test'}
+        payload = {'email': 'vasas@cisco.com', 'password': 'pw', 'name': 'Vasanth'}
         res = self.client.post(CREATE_USER_URL, payload)
-
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
         user_exists = get_user_model().objects.filter(
             email=payload['email']
         ).exists()
@@ -56,7 +56,7 @@ class PublicUserApiTests(TestCase):
 
     def test_create_token_for_user(self):
         """Test that a token is created for the user"""
-        payload = {'email': 'test@londonappdev.com', 'password': 'testpass'}
+        payload = {'email': 'vasas@cisco.com', 'password': 'password123'}
         create_user(**payload)
         res = self.client.post(TOKEN_URL, payload)
 
@@ -65,63 +65,68 @@ class PublicUserApiTests(TestCase):
 
     def test_create_token_invalid_credentials(self):
         """Test that token is not created if invalid credentials are given"""
-        create_user(email='test@londonappdev.com', password='testpass')
-        payload = {'email': 'test@londonappdev.com', 'password': 'wrong'}
+        create_user(email='vasas@cisco.com', password='password123')
+        payload = {'email': 'vasas@cisco.com', 'password': 'wrong'}
         res = self.client.post(TOKEN_URL, payload)
 
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_create_token_no_user(self):
-        """Test that token is not created if user doens't exist"""
-        payload = {'email': 'test@londonappdev.com', 'password': 'testpass'}
+        """Test that token is not created if user doesnt exist"""
+        payload = {'email': 'vasas@cisco.com', 'password': 'password123'}
         res = self.client.post(TOKEN_URL, payload)
 
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_create_token_missing_field(self):
-        """Test that email and password are required"""
+        """Test that email and password required"""
         res = self.client.post(TOKEN_URL, {'email': 'one', 'password': ''})
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_retrieve_user_unauthorized(self):
+        """Test that authentication is required for users"""
         res = self.client.get(ME_URL)
+
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
 class PrivateUserApiTests(TestCase):
+    """Test API requests that require authentication"""
 
-    def setUp(self) -> None:
+    def setUp(self):
         self.user = create_user(
-            email="test@mike.nl",
-            password='1234',
-            name='Name'
+            email='vasas@cisco.com',
+            password='password123',
+            name='Vasanth'
         )
         self.client = APIClient()
-        self.client.force_login(user=self.user)
+        self.client.force_authenticate(user=self.user)
 
     def test_retrieve_profile_success(self):
+        """Test retrieving profile for logged in user"""
         res = self.client.get(ME_URL)
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, {
-            'name':self.user.name,
-            'email':self.user.email
+            'name': self.user.name,
+            'email': self.user.email
         })
 
-    def test_post_me_not_allowed(self):
-        res = self.client.post(ME_URL,{})
+    def test_post_not_allowed(self):
+        """Test that POST is not allowed on the ME URL"""
+        res = self.client.post(ME_URL, {})
+
         self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
-    def test_user_update_profile(self):
-        payload = {'name':'newName', 'password':'4321'}
+    def test_update_user_profile(self):
+        """Test updating the user profile for authenticated user"""
+        payload = {'name': 'new name', 'password': 'newpassword123'}
         res = self.client.patch(ME_URL, payload)
-
         self.user.refresh_from_db()
+
         self.assertEqual(self.user.name, payload['name'])
         self.assertTrue(self.user.check_password(payload['password']))
         self.assertEqual(res.status_code, status.HTTP_200_OK)
-
-
